f21d6016e6828a2249eaebad4b3ed49ffba101ad
M G Asheque Mahmood <mahmood.asheque@gmail.com>
2023-09-17 22:00:35 +0300

    starting to use view templates

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
-import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
+import { configure, renderFile, serve } from "./deps.js";
+// import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
 
-const sql = postgres({});
-const handleRequest = async (request) => {
-  const rows = await sql`SELECT COUNT(*) AS c FROM users`;
-  return new Response(`Meaning of life: ${rows[0].c}`);
+// const sql = postgres({});
+// const handleRequest = async (request) => {
+//   const rows = await sql`SELECT COUNT(*) AS c FROM users`;
+//   return new Response(`Meaning of life: ${rows[0].c}`);
+// };
+
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
+
+const data = {
+  count: 0,
+};
+
+const handleRequest = async (request) => {
+  const url = new URL(request.url);
+  if (url.pathname === "/count") {
+    data.count++;
+    return new Response(await renderFile("count.eta", data), responseDetails);
+  }
+
+  return new Response("Hello you!");
+};
+
+
 serve(handleRequest, { port: 7777 });
