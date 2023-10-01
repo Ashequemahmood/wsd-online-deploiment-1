233efe597cb97275da957285a54194e7f0bd5b93
M G Asheque Mahmood <mahmood.asheque@gmail.com>
2023-10-01 16:46:52 +0300

    counting rows in database

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-import { configure, renderFile, serve } from "./deps.js";
-// import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
-
-// const sql = postgres({});
-// const handleRequest = async (request) => {
-//   const rows = await sql`SELECT COUNT(*) AS c FROM users`;
-//   return new Response(`Meaning of life: ${rows[0].c}`);
-// };
+import { serve } from "./deps.js";
+import { configure, renderFile } from "./deps.js";
+import { sql } from "./database/database.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
@@ -21,16 +16,22 @@ const data = {
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/visits") {
+  if (url.pathname === "/count") {
     data.count++;
     return new Response(await renderFile("count.eta", data), responseDetails);
-  }else if(url.pathname === "/meaning"){
-    return new Response("Seeking truths beyond meaning of life, you will find 43.");
-  }else{
-    return new Response("Nothing here yet.");
   }
 
-};
+  if (url.pathname === "/addresses") {
+    const rows = await sql`SELECT COUNT(*) as count FROM addresses`;
+    let rowCount = -42;
+    if (rows.length > 0) {
+      rowCount = rows[0].count;
+    }
 
+    return new Response(`Total rows: ${rowCount}`);
+  }
+
+  return new Response("Hello you!");
+};
 
 serve(handleRequest, { port: 7777 });
